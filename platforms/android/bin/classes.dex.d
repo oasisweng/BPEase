a692b9d1ba67c1b016f7b5c4b5159558e35960d0
oasisweng <dingzhong.weng@lycheestudio.co.uk>
2014-03-15 13:56:55 +0000

    delete platforms

--- platforms/android/bin/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/bin/classes.dex : \
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/bin/classes/com/doc/bpease/BPEase.class \
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/bin/classes/com/doc/bpease/BuildConfig.class \
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/bin/classes/com/doc/bpease/R$attr.class \
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/bin/classes/com/doc/bpease/R$drawable.class \
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/bin/classes/com/doc/bpease/R$string.class \
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/bin/classes/com/doc/bpease/R$xml.class \
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/bin/classes/com/doc/bpease/R.class \
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/bin/dexedLibs/classes-0f17ecc15b57c1fa25302ceaf312918d.jar \
