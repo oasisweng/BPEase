ce991551b4fbab3181a85ffb4a5eae51d77a7ba6
oasisweng <dingzhong.weng@lycheestudio.co.uk>
2014-04-06 18:03:52 -0400

    adding jspdf and jsplot

--- www/js/index.js
+++ www/js/index.js
@@ -16,39 +16,50 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 var app = {
     // Application Constructor
-    initialize: function () {
+    initialize: function() {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
+    bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
+    onDeviceReady: function() {
         //navigator.notification.alert("PhoneGap is ready!");
         logit("Phonegap is ready.");
+        logit("File system demo:");
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 1024, gotFS, onError);
         var element = document.getElementById('deviceProperties');
-        element.innerHTML = 'Device Name: '     + device.name     + '<br />' +
-                            'Device Cordova: '  + device.cordova  + '<br />' +
-                            'Device Platform: ' + device.platform + '<br />' +
-                            'Device UUID: '     + device.uuid     + '<br />' +
-                            'Device Model: '    + device.model    + '<br />' +
-                            'Device Version: '  + device.version  + '<br />';
+        element.innerHTML = 'Device Name: ' + device.name + '<br />' + 'Device Cordova: ' + device.cordova + '<br />' + 'Device Platform: ' + device.platform + '<br />' + 'Device UUID: ' + device.uuid + '<br />' + 'Device Model: ' + device.model + '<br />' + 'Device Version: ' + device.version + '<br />';
+        logit("setting local notificaition");
+        window.plugin.notification.local.onadd = function(id, state, json) {
+            logit("added a new local notification " + id + " " + state + " " + json);
+        };
+        window.plugin.notification.local.ontrigger = function(id, state, json) {
+            logit("a notification is triggered " + id + " " + state + " " + json);
+        };
+        window.plugin.notification.local.add({
+            message: 'Great app!'
+        });
         var title = 'Reminder';
         var message = 'Dont forget to buy some flowers.';
         var repeat = 'weekly';
         var date = new Date().getTime();
-        setLocalNotificaiton(new Date(date+6*1000),title,message,repeat);
+        var new_date = new Date(date + 6 * 1000);
+        setLocalNotificaiton(new_date, title, message, repeat);
+        generatePDFReport();
+
+        $('#pi-save-btn').bind("click", function(){
+            
+        })
     }
 };
 
@@ -61,30 +72,31 @@ function onError(err) {
     logit(err.code);
 }
 
-
 //==============================
 // Local Notification
 //==============================
-function setLocalNotificaiton(date_c,title_c,message_c,repeat_c){
-    var id_c = parseInt(Math.random()*1000);
-
+function setLocalNotificaiton(date_c, title_c, message_c, repeat_c) {
+    var id_c = parseInt(Math.random() * 1000);
+    logit("Notification ID " + id_c);
     window.plugin.notification.local.add({
-        id:      id_c,
-        title:   title_c,
+        id: id_c,
+        title: title_c,
         message: message_c,
-        repeat:  repeat_c,
-        date:    date_c
+        repeat: repeat_c,
+        date: date_c,
     });
 }
-
 //==============================
 // Files
 //==============================
-
 var FILENAME = "readme.txt",
     file = {
-        writer: { available: false },
-        reader: { available: false },
+        writer: {
+            available: false
+        },
+        reader: {
+            available: false
+        },
         fullPath: ""
     };
 
@@ -94,41 +106,43 @@ function gotFileWriter(fileWriter) {
     file.writer.object = fileWriter;
     saveText();
 }
+
 function gotFileEntry(fileEntry) {
     file.entry = fileEntry;
     file.fullPath = fileEntry.fullPath;
-    logit("Got File Entry! "+ file.fullPath);
+    logit("Got File Entry! " + file.fullPath);
     fileEntry.createWriter(gotFileWriter, onError);
 }
 
 function gotFS(fs) {
-    fs.root.getFile(FILENAME, {create: true, exclusive: false},
-                    gotFileEntry, onError);
+    fs.root.getFile(FILENAME, {
+        create: true,
+        exclusive: false
+    }, gotFileEntry, onError);
 }
 
 function readText() {
     if (file.entry) {
-        file.entry.file(function (txtFile) {
+        file.entry.file(function(txtFile) {
             var reader = new FileReader();
             file.reader.available = false;
-            reader.onloadend = function (evt) {
+            reader.onloadend = function(evt) {
                 file.reader.available = true;
                 var text = evt.target.result;
-                logit("Reading "+ text);
+                logit("Reading " + text);
                 //sendEmail();
             }
             reader.readAsText(txtFile);
         }, onError);
     }
-
     return false;
 }
-  
+
 function saveText() {
     if (file.writer.available) {
-        file.writer.available = false; 
+        file.writer.available = false;
         logit("saving text 'hello world!'");
-        file.writer.object.onwriteend = function (evt) {
+        file.writer.object.onwriteend = function(evt) {
             file.writer.available = true;
             file.writer.object.seek(0);
             logit("saving complete");
@@ -138,10 +152,8 @@ function saveText() {
     } else {
         logit("Writer is unavaiable!");
     }
-
     return false;
 }
-
 //==============================
 // Email Composer
 //==============================
@@ -153,34 +165,66 @@ function sendEmail() {
     ccRecipients = ["chaitanya.agrawal.13@ucl.ac.uk"];
     bccRecipients = [""];
     isHtml = true;
-    logit("Sending with Attachments:"+ file.fullPath);
-    attachments = [file.fullPath];   
+    logit("Sending with Attachments:" + file.fullPath);
+    attachments = [file.fullPath];
     attachmentsData = null;
-    window.plugins.emailComposer.showEmailComposerWithCallback(
-        sendEmail_Result,
-        subject,
-        body,
-        toRecipients,
-        ccRecipients,
-        bccRecipients,
-        isHtml,
-        attachments,
-        attachmentsData);
+    window.plugins.emailComposer.showEmailComposerWithCallback(sendEmail_Result, subject, body, toRecipients, ccRecipients, bccRecipients, isHtml, attachments, attachmentsData);
 }
 
 function sendEmail_Result() {
     logit("Email calling back");
 }
-
-
-
 //==============================
 // Datepicker
 //==============================
-$(function(){
+$(function() {
     $('#datepicker').datepicker({
-      format: 'mm-dd-yyyy',
-      endDate: '+1y',
-      startDate: '-80y'
+        format: 'mm-dd-yyyy',
+        endDate: '+1y',
+        startDate: '-80y'
     });
 });
+//==============================
+// PDF Generator
+// For more information, check: http://parall.ax/products/jspdf
+//==============================
+function generatePDFReport() {
+    //FIRST GENERATE THE PDF DOCUMENT
+    logit("generating pdf...");
+    var doc = new jsPDF();
+    doc.text(20, 20, 'HELLO!');
+    doc.setFont("courier");
+    doc.setFontType("normal");
+    doc.text(20, 30, 'This is a PDF document generated using JSPDF.');
+    doc.text(20, 50, 'YES, Inside of PhoneGap!');
+    var pdfOutput = doc.output();
+    logit(pdfOutput);
+    //NEXT SAVE IT TO THE DEVICE'S LOCAL FILE SYSTEM
+    logit("saving to file system...");
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+        logit(fileSystem.name);
+        logit(fileSystem.root.name);
+        logit(fileSystem.root.fullPath);
+        fileSystem.root.getFile("test.pdf", {
+            create: true
+        }, function(entry) {
+            var fileEntry = entry;
+            logit(entry);
+            entry.createWriter(function(writer) {
+                writer.onwrite = function(evt) {
+                    logit("write success");
+                };
+                logit("writing to file");
+                writer.write(pdfOutput);
+            }, function(error) {
+                logit(error);
+            });
+        }, function(error) {
+            logit(error);
+        });
+    }, function(event) {
+        logit(evt.target.error.code);
+    });
+}
+//Stringify a JSON JSON.stringify({test:123})
+//Parse a JSON String JSON.parse(json).test
