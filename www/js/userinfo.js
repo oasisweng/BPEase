3dd90a64508420db728cd098743ae3a6ff7638fc
oasisweng <dingzhong.weng@lycheestudio.co.uk>
2014-04-16 15:01:52 +0100

    add userinfo.js

--- www/js/userinfo.js
+++ www/js/userinfo.js
@@ -1,7 +1,39 @@
-$(document).live('pageshow', function(){
-   alert("pageshow");
-});
-
-function onLoad(){
-	alert("load");
+function saveUserInfo(){
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+        logit(fileSystem.name);
+        logit(fileSystem.root.name);
+        logit(fileSystem.root.fullPath);
+        fileSystem.root.getFile("test.pdf", {
+            create: true
+        }, function(entry) {
+            var fileEntry = entry;
+            logit(entry);
+            entry.createWriter(function(writer) {
+                writer.onwrite = function(evt) {
+                    logit("write success");
+                };
+                logit("writing to file");
+                writer.write(pdfOutput);
+            }, function(error) {
+                logit(error);
+            });
+        }, function(error) {
+            logit(error);
+        });
+    }, function(event) {
+        logit(evt.target.error.code);
+    });
 }
+
+
+//JSON.stringify
+//JSON.stringify({ uno: 1, dos : 2 }, null, '\t')
+// returns the string:
+// '{            \
+//     "uno": 1, \
+//     "dos": 2  \
+// }'
+
+// obj = $.parseJSON(data) 
+// obj.uno 
+// obj.dos
