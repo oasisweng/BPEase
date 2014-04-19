4df1627d112894304c8eff6610009ed5389b8b2e
Delia <deliaasuka@hotmail.com>
2014-04-20 00:35:55 +0100

    file reader writer to test

--- www/js/userinfo.js
+++ www/js/userinfo.js
@@ -1,30 +1,67 @@
 function saveUserInfo(){
-		var dob = $("#dob").value;
-		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
-        logit(fileSystem.name);
-        logit(fileSystem.root.name);
-        logit(fileSystem.root.fullPath);
-        fileSystem.root.getFile("userinfo.txt", {
-            create: true
-        }, function(entry) {
-            var fileEntry = entry;
-            logit(entry);
-            entry.createWriter(function(writer) {
-                writer.onwrite = function(evt) {
-                    logit("write success");
-                };
-                logit("writing to file");
-                writer.write(pdfOutput);
-            }, function(error) {
-                logit(error);
-            });
+    logit("can read this...");
+    var userinfo = {}
+    userinfo.name = $("#name").value;
+    userinfo.dob = $("#datepicker").value;
+    userinfo.nhsno = $("#nhsono").value;
+    userinfo.gpemail= $("#gpemail").value;
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+    logit(fileSystem.name + "yo");
+    logit(fileSystem.root.name + "yo2");
+    logit(fileSystem.root.fullPath + "yo3");
+    fileSystem.root.getFile("userinfo.txt", {
+        create: true
+    }, function(entry) {
+        logit(entry);
+        entry.createWriter(function(writer) {
+            writer.onwrite = function(evt) {
+                logit("write success");
+            };
+            logit("writing to file");
+            var json = JSON.stringify(userinfo); 
+            writer.write(json);
         }, function(error) {
             logit(error);
         });
-    }, function(event) {
-        logit(evt.target.error.code);
+    }, function(error) {
+        logit(error);
     });
+}, function(event) {
+    logit(evt.target.error.code);
+});
 }
+
+function readUserInfo(){
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+    logit(fileSystem.name);
+    logit(fileSystem.root.name);
+    logit(fileSystem.root.fullPath);
+    fileSystem.root.getFile("userinfo.txt", function(entry) {
+        var reader = new FileReader();
+        logit(entry);
+        reader.onloadend= function(evt){
+            var userinfo =evt.target.result;
+            var userinfoObj = JSON.parse(userinfo);
+            logit("reading"+userinfo);
+            document.getElementById("outname").innerHTML=userinfoObj.name;
+        }
+    }, function(error) {
+        logit(error);
+    });
+}, function(event) {
+    logit(event.target.error.code);
+});
+}
+
+ $("#next").bind("click",function(event){
+    saveUserInfo();
+}
+
+ $("#pi-save-btn").bind("click",function(event){
+    readUserInfo();
+
+ }
 
 
 //JSON.stringify
