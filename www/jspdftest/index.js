2daa8cd592fd19f3abf791921403f68c40233a84
Chaitanya Agrawal <chaitanya.agrawal.13@ucl.ac.uk>
2014-04-21 23:55:39 +0100

    pdfreport

--- www/jspdftest/index.js
+++ www/jspdftest/index.js
@@ -70,8 +70,6 @@ function generatePDFReport(date_time, systolic, diastolic, pulse, personalDetais
         content2+= '</tr>';
     }
 
-    
-
 
 
 //======================================
@@ -90,22 +88,3 @@ function generatePDFReport(date_time, systolic, diastolic, pulse, personalDetais
 
 
 
-
-var content = "<tr>";
-for (var a = 0; a < systolic.length; a++) {
-    for (var b = 0; b < date_and_time.length; b += 2) {
-        content += '<td>' + date_and_time[b] + '</td>';
-        content += '<td>' + date_and_time[b + 1] + '</td>';
-        for (var c = 0; c < systolic.length; c++) {
-            content += '<td>' + systolic[c] + '</td>';
-            for (var d = 0; d < diastolic.length; d++) {
-                content += '<td>' + diastolic[d] + '</td>';
-                for (var e = 0; e < pulse.length; e++) {
-                    content += '<td>' + pulse[e] + '</td>';
-                    content += '</tr>';
-                }
-            }
-        }
-    }
-}
-$("#bp-table").innerHTML += content;
