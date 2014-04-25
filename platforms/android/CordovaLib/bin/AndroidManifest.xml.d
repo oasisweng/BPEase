d444d5f1091f747d295c5d51b95cd10107444264
oasisweng <dingzhong.weng@lycheestudio.co.uk>
2014-04-25 18:23:00 +0100

    remove excessive files

--- platforms/android/CordovaLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
-/Users/oasis_weng/Desktop/repos/app/BPEase/platforms/android/CordovaLib/AndroidManifest.xml \
